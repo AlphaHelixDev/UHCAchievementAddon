2cb5aa20e0a17665ff0adce39bb0a6506b650fae
AlphaHelixDev <max.hertel@outlook.de>
2017-06-17 16:14:45 +0200

    Optimized inventory

--- src/main/java/de/alphahelix/achievementaddon/inventories/AchievementInventory.java
+++ src/main/java/de/alphahelix/achievementaddon/inventories/AchievementInventory.java
@@ -3,6 +3,7 @@ package de.alphahelix.achievementaddon.inventories;
 import de.alphahelix.achievementaddon.AchievementAddon;
 import de.alphahelix.achievementaddon.instances.Achievement;
 import de.alphahelix.alphalibary.inventorys.InventoryBuilder;
+import de.alphahelix.alphalibary.utils.Util;
 import de.alphahelix.uhcremastered.UHC;
 import de.alphahelix.uhcremastered.enums.GState;
 import de.alphahelix.uhcremastered.utils.StatsUtil;
@@ -11,8 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 
@@ -22,7 +21,7 @@ import static de.alphahelix.achievementaddon.AchievementAddon.getAchievementOpti
 
 public class AchievementInventory {
 
-    private static WeakHashMap<String, Inventory> inventories = new WeakHashMap<>();
+    private static WeakHashMap<String, Inventory> inventoryMap = new WeakHashMap<>();
 
     public AchievementInventory() {
         Bukkit.getPluginManager().registerEvents(new Listener() {
@@ -30,8 +29,7 @@ public class AchievementInventory {
             public void onClick(PlayerInteractEvent e) {
                 if (!GState.is(GState.LOBBY)) return;
                 if (e.getItem() == null) return;
-                if (e.getItem().getType() != AchievementAddon.getAchievementOptions().getIcon().getItemStack().getType())
-                    return;
+                if(Util.isSame(e.getItem(), AchievementAddon.getAchievementOptions().getIcon().getItemStack())) return;
 
                 openInv(e.getPlayer());
             }
@@ -40,36 +38,27 @@ public class AchievementInventory {
 
     private void openInv(Player p) {
 
-        if (inventories.containsKey(p.getName())) {
-            p.openInventory(inventories.get(p.getName()));
+        if(inventoryMap.containsKey(p.getName())) {
+            p.openInventory(inventoryMap.get(p.getName()));
             return;
         }
 
-        InventoryBuilder ib = new InventoryBuilder(getAchievementOptions().getGuiName(), ((8 / 9) + 1) * 9) {
-            @Override
-            public void onOpen(InventoryOpenEvent inventoryOpenEvent) {
-            }
-
-            @Override
-            public void onClose(InventoryCloseEvent inventoryCloseEvent) {
-            }
-        };
+        InventoryBuilder ib = new InventoryBuilder(
+                p, getAchievementOptions().getGuiName(), ((8 / 9) + 1) * 9);
 
         for (int i = 0; i < 7; i++) {
             Achievement a = Achievement.getAchievement(i);
-            ib.addItem(new InventoryBuilder.SimpleItem(a.getIcon(StatsUtil.getStatistics(p).hasCustomStatistic(a), true), i) {
-                @Override
-                public void onClick(InventoryClickEvent inventoryClickEvent) {
-                    if (inventoryClickEvent.getClickedInventory().getTitle().equals(getAchievementOptions().getGuiName())) {
 
-                        inventoryClickEvent.setCancelled(true);
-                    }
+            ib.addItem(ib.new SimpleItem(a.getIcon(StatsUtil.getStatistics(p).hasCustomStatistic(a), true), i) {
+                @Override
+                public void onClick(InventoryClickEvent e) {
+                    e.setCancelled(true);
                 }
             });
         }
 
-        inventories.put(p.getName(), ib.build());
-        p.openInventory(inventories.get(p.getName()));
+        inventoryMap.put(p.getName(), ib.build());
+        p.openInventory(inventoryMap.get(p.getName()));
     }
 
 }
